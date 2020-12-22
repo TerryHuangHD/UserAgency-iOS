eaaf079710615ad447359ec00cb6e19ae3c34bae
Terry <terryhuanghd@gmail.com>
2020-12-22 16:22:13 +0800

    Test Init

---
+++ UserAgency/UserAgency.h
@@ -0,0 +1,18 @@
+//
+//  UserAgency.h
+//  UserAgency
+//
+//  Created by Terry Huang on 2020/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserAgency.
+FOUNDATION_EXPORT double UserAgencyVersionNumber;
+
+//! Project version string for UserAgency.
+FOUNDATION_EXPORT const unsigned char UserAgencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserAgency/PublicHeader.h>
+
+
---
+++ UserAgencyExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserAgencyExample
+//
+//  Created by Terry Huang on 2020/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
